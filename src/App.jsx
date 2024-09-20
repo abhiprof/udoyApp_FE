ed74f86dc7f906ae109432f206cfa5f79613d1dd
Abhishek Anand <abhishek.anand7@globallogic.com>
2024-09-20 19:48:35 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useDebugValue, useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import axios from 'axios'
+
+function App() {
+  const [jokes,setJokes]=useState([])
+
+  useEffect(()=>{
+    axios.get('')
+  },[])
+
+  return (
+    <>
+      <div>
+      {jokes.map((joke,index)=>{
+        <h3>{joke.title}</h3>
+      })}
+      </div>
+    </>
+  )
+}
+
+export default App
