b04ebf25504aa2ba43b942e1b4d04133b96aabb9
Abhishek Anand <abhishek.anand7@globallogic.com>
2024-09-21 10:46:09 +0530

    correcting vite config

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-export default {
+export default defineConfig({
+  plugins: [react()],
   server: {
-    port: 8080,
+    port: 8080,  // Your custom port
   },
-};
+});
