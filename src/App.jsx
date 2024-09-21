77fc12ac662572620f6d93bd7eaa61bab3963cc2
Abhishek Anand <abhishek.anand7@globallogic.com>
2024-09-21 10:38:44 +0530

    adding react

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import { useDebugValue, useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
 import './App.css'
-import axios from 'axios'
+
 
 function App() {
   const [jokes,setJokes]=useState([])
